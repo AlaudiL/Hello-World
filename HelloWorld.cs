6cb416bcd0eec9ec0a7dea534f444bf59188b13f
AlaudiL <lujiah97@gmail.com>
2020-08-16 13:13:41 +0800

    HelloWorld

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace gitHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
